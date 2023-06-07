a59abb01a070e1eb5b187480a16bfb88b1546acd
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-07 21:03:34 +0300

    another error correction

---
+++ Username_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "kirill")
+{
+    Console.WriteLine("You are the best! Hi man!");
+}
+else
+{
+    Console.Write("Hello! ");
+    Console.WriteLine(username);
+}
