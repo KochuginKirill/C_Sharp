36b2573e79ed7b9fd53124babbbc93fa591b66a6
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-07 20:36:00 +0300

    Summ modified with random

--- Summ2/Program.cs
+++ Summ2/Program.cs
@@ -1,5 +1,7 @@
-﻿int number1 = 2;
-int number2 = 3;
+﻿int number1 = new Random().Next(1, 10); // 1,2,3 ... 9
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 int summ = number1 + number2;
 Console.WriteLine("Сумма двух чисел равна");
 Console.Write(summ);
