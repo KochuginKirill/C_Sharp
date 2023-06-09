e8681a1b1704c15d74ce62e430c93d27b43d6310
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-09 21:43:38 +0300

    Seminar 1.4

---
+++ Seminar1_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите целое положительное");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = -number1;
+while (number2 <= number1)
+{
+      Console.Write($"{number2} ");
+      number2++;
+}
