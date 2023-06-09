a11269ed632f6d8d02933c934b5e3a04f004f68a
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-09 20:38:07 +0300

    Seminar 1.1

---
+++ Seminar1_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Напишите чичло");
+string input_string = Console.ReadLine();
+int number = Convert.ToInt32(input_string);
+int summ = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {summ}");
