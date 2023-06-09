637de409eb922b1f4b35ded222aa43514b6b32be
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-09 21:03:49 +0300

    Seminar 1.2

---
+++ Seminar1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Напишите первое число");
+string input_string1 = Console.ReadLine();
+Console.WriteLine("Напишите второе число");
+string input_string2 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = Convert.ToInt32(input_string2);
+if (number2 == number1 * number1)
+{
+    Console.WriteLine($"Число {number2} является квадратом {number1}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} не является квадратом {number1}");
+}
+
