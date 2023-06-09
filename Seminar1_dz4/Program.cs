0ea04cc06a72b2ef3b16cfea009805cca430c79a
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-09 22:38:17 +0300

    четвертое задание первого дз

---
+++ Seminar1_dz4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите целое положительное число");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int result = 2;
+while (result <= number1)
+{
+   Console.Write($"{result} ");
+   result += 2;
+}
+
+
