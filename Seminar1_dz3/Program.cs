5f9e0e53018a8cc10d5628152f1ebbec799e262c
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-09 22:27:40 +0300

    третье задание первого дз

---
+++ Seminar1_dz3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Напишите целое положительное число");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int result = number1%2;
+if (result == 0)
+{
+   Console.WriteLine($"Число {number1} является четным");
+}
+else 
+{
+    Console.WriteLine($"Число {number1} является нечетным");
+}
+
