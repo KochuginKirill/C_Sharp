0d78efef476bb3fcb1193f72b3f9ffb9fc41b793
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-13 16:21:59 +0300

    test added

---
+++ test/Program.cs
@@ -0,0 +1,28 @@
+﻿
+    void CreateNewArray(int[] NewArray)
+    {
+    Console.WriteLine($"Введите число элементов массива");
+    string StringInput1 = Console.ReadLine();
+    int length = Convert.ToInt32(StringInput1);
+    NewArray = new int[length];
+    int index = 0;
+    while(index <= length-1)
+    {
+        Console.WriteLine($"Напишите {index} элемент массива");
+        string StringInput2 = Console.ReadLine();
+        NewArray[index] = Convert.ToInt32(StringInput2);
+        index++;
+    }
+
+    int index2 = 0;
+
+    while(index2 <= length-1)
+    {
+        Console.WriteLine($"{index2} элемент массива равен: {NewArray[index2]}");
+        index2++;
+    }
+    }
+int[] SuperArray = new int[0];
+CreateNewArray(SuperArray);
+int[] UltraArray = new int[0];
+CreateNewArray(UltraArray);
