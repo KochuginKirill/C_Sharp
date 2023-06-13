5db425d638edb54f4ac27b7877c4cd2745367018
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-13 21:31:12 +0300

    Seminar2_4 added

---
+++ Seminar2_4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число 
+//и проверяет, кратно ли оно одновременно 7 и 23.
+//14 -> нет 
+//46 -> нет 
+//161 -> да
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое число");
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"{number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"{number} не кратно 7 и 23");
+}
