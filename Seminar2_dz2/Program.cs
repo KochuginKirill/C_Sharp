f66d213312745473033a632775e1f333a3de5891
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-14 10:35:55 +0300

    Seminar2_dz2 added

---
+++ Seminar2_dz2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, 
+//которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое число");
+
+if(number > 100)
+{
+    while(number > 1000)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine($"Третья цифра: {number}");
+}
+else
+{
+    Console.WriteLine($"Число {number} не содержит третьей цифры");
+}
