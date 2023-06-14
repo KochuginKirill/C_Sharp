fa01282319e4cbad5b7baacfefe8ba897248549c
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-14 10:25:08 +0300

    Seminar2_dz1 added

---
+++ Seminar2_dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое трехзначное число");
+
+number = number / 10;
+number = number % 10;
+
+Console.WriteLine($"Вторая цифра: {number}");
