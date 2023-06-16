4c26b01018a27f9577b7b0d59ae87539dc4f3fa6
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-16 21:37:32 +0300

    Seminar3_3 and Seminar3_4 added

---
+++ Seminar3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int X1 = ReadInt("Введите координату X1");
+int Y1 = ReadInt("Введите координату Y1");
+int X2 = ReadInt("Введите координату X2");
+int Y2 = ReadInt("Введите координату Y2");
+
+double result = Math.Sqrt(Math.Pow((X2-X1),2) + Math.Pow((Y2-Y1),2));
+
+Console.WriteLine($"Расстояние между двумя точками равно: {result}");
