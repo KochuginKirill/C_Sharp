dee1aa9e57869fc0b3cde3c871acc15d70078311
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-16 20:14:42 +0300

    Seminar3_1 added

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = int.Rarse(Console.ReadLine());
+    return value;
+}
+
+bool ValidateCoords(int x, int y)
+{
+    if (x == 0 || y == 0)
+    {
+        Console>writeLine("одна из координат ровна нулю");
+        return false;
+    }
+    return true;
+}
+
