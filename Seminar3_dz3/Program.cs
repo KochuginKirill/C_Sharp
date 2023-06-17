75a5eaca4ba58e04453176b4690de5bb805eb2a6
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-17 10:55:12 +0300

    Seminar3_dz3 added

---
+++ Seminar3_dz3/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое положительное число");
+
+int index = 1;
+
+while(index <= number)
+{
+    int result = (index * index);
+    Console.Write($"{result} ");
+    index++;
+}
