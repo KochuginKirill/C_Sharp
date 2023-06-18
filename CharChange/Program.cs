05585da23bf373965d963535c285bda1f51bac45
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-18 12:58:20 +0300

    CharChange added

---
+++ CharChange/Program.cs
@@ -0,0 +1,14 @@
+﻿string Replace(string text, char oldValue, char NewValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int index = 0; index < length; index++)
+    {
+        if(text[index] == oldValue) result = result + $"{NewValue}";
+        else result = result + $"{text[index]}";
+    }
+    return result;
+}
+
+string NewText = Replace("qweqwe",'q','d');
+Console.Write(NewText);
