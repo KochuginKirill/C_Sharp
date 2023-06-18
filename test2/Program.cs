1acbdac1ec8fcbea6d5ff61b55d8ef529d490339
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-18 12:34:59 +0300

    test 2 added

---
+++ test2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+    int[] CreateNewArray(int ArrayLength)
+    {
+    int length = ArrayLength;
+    int[] NewArray = new int[length];
+    int index = 0;
+    while(index <= length-1)
+    {
+        Console.WriteLine($"Напишите {index} элемент массива");
+        string StringInput2 = Console.ReadLine();
+        NewArray[index] = Convert.ToInt32(StringInput2);
+        index++;
+    }
+ Console.WriteLine();
+    int index2 = 0;
+
+    while(index2 <= length-1)
+    {
+        Console.Write($"{NewArray[index2]} ");
+        index2++;
+    }
+    return NewArray;
+    }
+   int[] SuperArray = CreateNewArray(4); 
