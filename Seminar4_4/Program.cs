d7f1fa60899b84d1636201c47af033570793de25
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-20 21:41:27 +0300

    Seminar 4(4_5 added)

---
+++ Seminar4_4/Program.cs
@@ -0,0 +1,23 @@
+﻿ int[] CreateNewArray(int ArrayLength, int min, int max)
+    {
+    int length = ArrayLength;
+    int[] NewArray = new int[length];
+    int index = 0;
+    while(index <= length-1)
+    {
+        Random rnd = new Random();
+        NewArray[index] = rnd.Next(min, max);
+        index++;
+    }
+ Console.WriteLine();
+    int index2 = 0;
+
+    while(index2 <= length-1)
+    {
+        Console.Write($"{NewArray[index2]} ");
+        index2++;
+    }
+    return NewArray;
+    }
+   int[] SuperArray = CreateNewArray(8, 0, 2); 
+
