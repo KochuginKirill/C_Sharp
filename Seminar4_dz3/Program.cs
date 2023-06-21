b5e32d88ec23971539a91df899a190dc85c12ab1
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-21 14:23:43 +0300

    Seminar4_dz3 added

---
+++ Seminar4_dz3/Program.cs
@@ -0,0 +1,37 @@
+﻿int CheckInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string InputStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(InputStr, out value))
+    {
+        return value;
+    }
+    Console.WriteLine("Извините, но вы ввели не целое число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[] GenerateArray(string meassage)
+{
+    int len = CheckInt("Введите число элементов массива");
+    int min = CheckInt("Введите минимальную числовую границу элемента массива");
+    int max = CheckInt("Введите максимальную числовую границу элемента массива");
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+void PrintArrey(int[] Arrey)
+{
+    for (int i = 0; i < Arrey.Length; i++)
+    {
+        System.Console.Write(Arrey[i] + "\t");
+    }
+}
+
+int[] NewArray = GenerateArray();
