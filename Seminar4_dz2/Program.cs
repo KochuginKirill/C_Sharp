7fb47bce8a8d5952e54a3dc814847430af483977
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-21 11:31:04 +0300

    Seminar4_dz2 added

---
+++ Seminar4_dz2/Program.cs
@@ -0,0 +1,41 @@
+﻿int CheckInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string InputStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(InputStr, out value))
+    {
+        return value;
+    }
+    Console.WriteLine("Извините, но вы ввели не целое число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int len(int number)
+{
+     int count = 0;
+    for(int check = number; check > 0; count++)
+    {
+        check = check / 10;
+    }
+    return count;
+}
+
+int SumNumb(int NumberCount, int NewNumber)
+{
+    int sum = 0;
+    for(int index = 0; index < NumberCount; index++)
+    {
+        sum = sum + NewNumber % 10;
+        NewNumber = NewNumber / 10;    
+    }
+    return sum;
+}
+
+int NewNumber = CheckInt("Введите целое положительное число");
+int CountNumber = len(NewNumber);
+int result = SumNumb(CountNumber, NewNumber);
+Console.WriteLine($"сумма цифр числа {NewNumber} равняется {result}");
+
+
