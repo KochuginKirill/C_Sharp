746807d810e18bd032480c08881b7690a4d11bb1
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-23 20:57:06 +0300

    Seminar 5_1_2 added

---
+++ Seminar5_1/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] GenerateArray(int len, int min, int max)
+{
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+int[] CountPositivesSumNegatives(int[] input)
+    {
+      int minSum = input.Where(x => x > 0).Sum();
+      int maxSum = input.Where(x => x < 0).Sum();
+      int[] result = {maxSum, minSum};
+      return result;
+    }
+
+void PrintArray(int[] Arrey)
+{
+    for (int i = 0; i < Arrey.Length; i++)
+    {
+        System.Console.Write(Arrey[i] + "\t");
+    }
+}
+
+int[] NewArray = GenerateArray(12, -9, 9);
+PrintArray(NewArray);
+int[] megaSum = CountPositivesSumNegatives(NewArray);
+Console.WriteLine("Суммы равны: ");
+PrintArray(megaSum);
+
---
+++ Seminar5_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Напишите программу замены 
+//элементов массива: положительные элементы 
+//замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GenerateArray(int len, int min, int max)
+{
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+int [] ReverseArray(int[] input)
+{
+    for(int index = 0; index < input.Length; index++)
+    {
+        input [index] = input [index] * -1;
+    }
+    return input;
+}
+
+void PrintArray(int[] Arrey)
+{
+    for (int i = 0; i < Arrey.Length; i++)
+    {
+        System.Console.Write(Arrey[i] + "\t");
+    }
+}
+
+int[] newArray = GenerateArray(10, -10, 10);
+PrintArray(newArray);
+Console.WriteLine();
+int[] reverse = ReverseArray(newArray);
+PrintArray(reverse);
