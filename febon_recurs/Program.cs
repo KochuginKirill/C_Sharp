a5e752611f9ccda28b4bddb6d15b67a31ddcb29a
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-25 14:20:59 +0300

    febon_recurs added

---
+++ febon_recurs/Program.cs
@@ -0,0 +1,9 @@
+﻿double Fibonachi(int number)
+{
+    if(number == 1 || number == 2) return 1;
+    else return Fibonachi(number - 1) + Fibonachi(number - 2);
+}
+for(int i =1; i < 10; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonachi(i)}");
+}
