35e01ab295256e978e519ef96a9079ff69dbbf7e
Kirill_Kochugin <kraingor@yandex.ru>
2023-06-25 19:52:22 +0300

    tetris shanged rotate works

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -17,6 +17,47 @@
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
+int[,] iShape = new int[,]
+    {
+        {1},
+        {1},
+        {1},
+        {1},
+    };
+
+int[,] jShape = new int[,]
+{
+    {0 ,1},
+    {0 ,1},
+    {1 ,1},
+};
+
+int[,] lShape = new int[,]
+{
+    {1 ,0},
+    {1 ,0},
+    {1 ,1},
+};
+
+int[,] oShape = new int[,]
+{
+    {1 ,1},
+    {1 ,1},
+};
+
+int[,] zShape = new int[,]
+{
+    {1, 1, 0},
+    {0, 1, 1},
+};
+
+int[,] tShape = new int[,]
+{
+    {0, 1, 0},
+    {1, 1 ,1},
+};
+
+
 
 void Tetris(int[,] field)
 {   
@@ -24,20 +65,13 @@ void Tetris(int[,] field)
     {
         for(int j = 0; j < field.GetLength(1); j++)
         {
-            if(field[i,j] == 0) Console.Write($"-");
+            if(field[i,j] == 0) Console.Write($" ");
             else Console.Write($"o");
         }
         Console.WriteLine();
     }
 }
  
- int[,] iShape = new int[,]
-    {
-        {1},
-        {1},
-        {1},
-        {1},
-    };
 
 int [,] rotate(int[,] input)
 {
@@ -49,13 +83,18 @@ int [,] rotate(int[,] input)
         for(int j = 0; j < newMatrix.GetLength(1); j++)
         {
             newMatrix[i,j] = input[j, i];
+        
         }
     }
-
     return newMatrix;
 }
 
-Tetris(iShape);
-int[,] iShapeRotate = rotate(iShape);
-Tetris(iShapeRotate);
+Tetris(jShape);
+int[,] ShapeRotate = rotate(jShape);
+Tetris(ShapeRotate);
+ShapeRotate = rotate(ShapeRotate);
+Tetris(ShapeRotate);
+ShapeRotate = rotate(ShapeRotate);
+Tetris(ShapeRotate);
+
 
