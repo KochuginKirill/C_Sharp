03c68ae8bf439152de2e54c7996af9455d48e6f9
Kirill_Kochugin <kraingor@yandex.ru>
2023-07-08 13:17:27 +0300

    Seminar9_dz_added

---
+++ Seminar9_dz1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет 
+// все чётные натуральные числа 
+// в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void FromMToN (int m, int n)
+{
+    if(n < m)
+    {
+       return; 
+    } 
+    else 
+    {
+        FromMToN(m, n - 1);
+        if(n % 2 == 0)
+        {
+        Console.Write($"{(n)} ");
+        }
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int m = ReadInt("Введите число M => ");
+int n = ReadInt("Введите число N => ");
+FromMToN(m, n);
---
+++ Seminar9_dz2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int FromMToN (int m, int n, int sum = 0)
+{
+    if(n < m)
+    {
+       return sum; 
+    } 
+    else 
+    {
+        return FromMToN(m, n - 1, sum += n);
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int m = ReadInt("Введите число M => ");
+int n = ReadInt("Введите число N => ");
+int result = FromMToN(m, n);
+Console.WriteLine($"Сумма натуральных элементов в промежутке: {result}");
